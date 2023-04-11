3329f68347bc1d2cc145e3c3fb520dd8159b2255
Tsz Fung Chan <tszfung@Tszs-Mac-mini.local>
2023-04-11 00:20:57 -0700

    new tests to be added

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,17 +4,3 @@ from scipy import stats
 from hamsta import HAMSTA
 
 
-def test_fit():
-    U = np.load(".pytest_cache/example.SVD.U.npy")
-    S = np.load(".pytest_cache/example.SVD.S.npy")
-    S = S[S > 1e-3]
-    U = U[S > 1e-3]
-    h2a = 0.05
-    M = 8000
-
-    ham = HAMSTA()
-
-    rotated_Z = stats.norm.rvs(loc=0.0, scale=np.sqrt(h2a / M * S ** 4 + S ** 2))
-    ham.fit(rotated_Z=rotated_Z, U=U, S=S, M=M, jackknife=False, est_thres=True)
-
-    print(ham.to_dataframe())
--- tests/test_simu.py
+++ tests/test_simu.py
@@ -5,25 +5,3 @@ from tqdm import tqdm
 from hamsta import HAMSTA, io, preprocess, simu
 
 
-def test_simu():
-    LAmatrix, sample_df = io.read_zarr(
-        "../example_data/example.subset.concat.zarr/", "AFR"
-    )
-
-    Q = LAmatrix.mean(axis=0)[:, None] / 2
-
-    U, S = preprocess.SVD(A=LAmatrix, Q=Q)
-
-    pheno = simu.simu_pheno(A=LAmatrix, Q=Q, pve=jnp.array([5e-3, 0, 0]), rep=11)
-
-    tstat = simu.assoc(y=pheno, x=LAmatrix.T, c=Q)
-
-    ham = HAMSTA()
-
-    simu_results = []
-    for i in tqdm(range(10)):
-        ham.fit(Z=tstat[:, i], S=S, U=U, jackknife=False)
-        simu_results.append(ham.to_dataframe())
-
-    simu_results = pd.concat(simu_results)
-    print(simu_results)
